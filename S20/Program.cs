8000c7126acaff2973960a0fdd980e04ff14aba3
sarina <sarina.abbasii16@gmail.com>
2025-05-04 13:35:37 -0700

    add HW S20

---
+++ S20/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace S20;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string str = "Hello , World";
+        string encoded = str.Encoder();
+    Console.WriteLine(encoded); 
+
+    string decoded = encoded.Decoder();
+    Console.WriteLine(decoded);
+
+    }
+
+ 
+}
